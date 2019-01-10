f61a39285c2c63f48796f30f38867f68db792894
Ian Li <il.348963380@gmail.com>
2019-01-10 14:55:53 -0500

    Jan.10.19 update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+# ^ Don't mind this thing it's just for ian 
+
 include system
 include time
 include pygame
 
+# main function
 def main():
-
     print("no I hate this world.")
 
 
