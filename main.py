a1088e3c305e0a6792de12d4d07fa85b12ca2b45
Ian Li <il.348963380@gmail.com>
2019-01-10 14:56:37 -0500

    Jan.10.19 update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # ^ Don't mind this thing it's just for ian 
 
-include system
-include time
-include pygame
+import system
+import time
+import pygame
 
 # main function
 def main():
     print("no I hate this world.")
 
-
+# de facto crap
 if __name__ == '__main__':
     main()
