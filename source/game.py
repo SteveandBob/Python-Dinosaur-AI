ca993558461e16208fb2db7ce5d1c167d5c2170c
Shawn Hu <wwxyz.9999@gmail.com>
2019-01-11 10:43:13 -0500

    created obstacle class

--- source/game.py
+++ source/game.py
@@ -10,6 +10,13 @@ jumpIntervals = [4,4,3,3,3,2,2,2,2,1,1,1,-1,-1,-1,-2,-2,-2,-2,-3,-3,-3,-4,-4]
 playerX = 0
 playerY = 0
 
+class obstacle:
+    def __init__(self, speed, uniqueID):
+        self.speed = speed
+        self.ID = uniqueID
+    def getID(self):
+        return self.ID
+
 def jump:
     if(touchingGround == False):
         continue()
@@ -28,4 +35,5 @@ def mainGame():
     for event in pygame.event.get():
         if(event.Type == KEYUP):
             thread.start_new_thread(jump)
+    
     return
