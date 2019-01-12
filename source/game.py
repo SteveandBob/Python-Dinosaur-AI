98c6c05bf8ec096d9bc7ffdf1e4afac1b012ef94
Ian Li <il.348963380@gmail.com>
2019-01-12 16:58:24 -0500

    Jan.12.19 fixed syntax issues, code runs now

--- source/game.py
+++ source/game.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+# shebang for linux/mac/unix users
+
 import sys
 import time
 import pygame
-import threading
+# import threading
 import random
-from pygame import *
-#begin programming the game
+# from pygame import *
+
+# begin programming the game
 
 touchingGround = True
-jumpIntervals = [10,10,6,6,6,3,3,3,3,2,2,1,-1,-2,-2,-3,-3,-3,-3,-6,-6,-6,-10,-10]
+jumpIntervals = [10, 10, 6, 6, 6, 3, 3, 3, 3, 2, 2, 1, -1, -2, -2, -3, -3, -3, -3, -6, -6, -6, -10, -10]
 
 spawn1 = False
 spawn2 = False
@@ -30,15 +34,27 @@ delay = 0
 
 pygame.init()
 screen = pygame.display.set_mode((screenX, screenY))
+
+
 def refreshScreen():
-    screen.fill((255,255,255))
+    screen.fill((255, 255, 255))
     return
 
+
 def moveBlocks():
+    # you forgot this
+    global block1X, block2X
+    # however, this is the wrong way of passing in global variables in python
+    # what it should be instead is that we pass in the global variables
+    # then, when we call it, we call `block1X = moveBlocks(block1X)`
+    # and then call `block2X = moveBlocks(block2X)`
+    # but, I am a lazy bastard, and hence I put `global block1X, block2X`
+    # TODO #1 - FIX THIS LAZY SLOP OF A HACK
+    # - ian
     block1X -= speed
     block2X -= speed
-    pygame.draw.rect(screen, (0,0,0), [block1X, initY, initX + blockW, initY + blockH])
-    pygame.draw.rect(screen, (0,0,0), [block2X, initY, initX + blockW, initY + blockH])
+    pygame.draw.rect(screen, (0, 0, 0), [block1X, initY, initX + blockW, initY + blockH])
+    pygame.draw.rect(screen, (0, 0, 0), [block2X, initY, initX + blockW, initY + blockH])
     if(block1X <= -50):
         delay = random.randint(minTime, maxTime)
         time.wait(delay)
@@ -48,20 +64,31 @@ def moveBlocks():
         time.wait(delay)
         block2X = 900
 
+
 def drawPlayer():
-    pygame.draw.rect(screen, (255,0,0), [playerX, playerY, playerX + width, playerY + height])
+    pygame.draw.rect(screen, (255, 0, 0), [playerX, playerY, playerX + width, playerY + height])
     return
 
+
 def jump():
-    if(touchingGround == True):
+    global playerY
+    # TODO #2 - FIX STATEMENT ABOVE
+    # SEE TODO #1
+    # - ian
+    if(touchingGround):
         for i in jumpIntervals:
             playerY += i
-            pygame.draw.rect(screen, (255,0,0), [playerX, playerY, playerX + width, playerY + height])
+            pygame.draw.rect(screen, (255, 0, 0), [playerX, playerY, playerX + width, playerY + height])
             time.wait(20)
     return
 
+
 def collisionDetect():
-    if((playerX + width) > (blockX1 or blockX2) and playerY < (initY + blockH) and playerX < (blockX1 or blockX2)):
+    global playerY, touchingGround
+    # TODO #3 - FIX STATEMENT ABOVE
+    # SEE TODO #1
+    # - ian
+    if((playerX + width) > (block1X or block2X) and playerY < (initY + blockH) and playerX < (block1X or block2X)):
         quit()
     if(playerY <= 350):
         playerY = 350
@@ -69,14 +96,15 @@ def collisionDetect():
     else:
         touchingGround = False
 
+
 def mainGame():
     while(True):
         refreshScreen()
         if(pygame.event.get()):
-            if(event.type == pygame.key.get_pressed()):
-                if(event.key == K_UP):
+            if(pygame.event.EventType == pygame.key.get_pressed()):
+                if(pygame.event.key == pygame.K_UP):
                     jump()
-             elif(event.type == QUIT):
+            elif(pygame.event.EventType == pygame.QUIT):
                 pygame.quit()
                 sys.exit()
         else:
