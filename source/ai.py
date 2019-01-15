18227ce577b116f7a82bc3798f19aa42db9478f0
Shawn Hu <wwxyz.9999@gmail.com>
2019-01-15 15:55:26 -0500

    Jan.15.19 Started AI, Input layers established

--- source/ai.py
+++ source/ai.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
 import sys
 import time
+import game
+
+#variables
+playerSpeed = 0
+onGround = 1
+blockDist = 0
+nextBlockPos = 0
+
+#global constants
+
+
+
+#input layers
+playerSpeed = #game.player.speed
+if(#game.player.touchingground = true):
+    onGround = 1
+else:
+    onGround = 0
+if(#game.player.block1X < block2X):
+    nextBlockPos = block1X
+else:
+    nextBlockPos = block2X
+blockDist = nextBlockPos - #game.player.playerX
+
+#hidden nodes
+
+
+#output layer
