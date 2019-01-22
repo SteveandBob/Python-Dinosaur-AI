9a1d4715da2f95585bd1f16fd41611376aafc0da
Shawn Hu <wwxyz.9999@gmail.com>
2019-01-22 14:45:39 -0500

    AI works, time to make it smart

--- source/main.py
+++ source/main.py
@@ -2,14 +2,13 @@
 import pygame
 import sys
 import threading
-import builtins
+import random
 
 pygame.init()
 scoreFont = pygame.font.SysFont('Times New Roman', 14)
 screen = pygame.display.set_mode((1000, 300))
 groundHeight = 210
 currentScore = 0
-builtins.score = currentScore
 delay = 100
 
 class enemy:
@@ -103,11 +102,83 @@ def scoreCounter(block1, block2, delay):
                     delay = 5
         pygame.time.wait(delay)
 
+class ai:
+    def __init__(self):
+        self.aiScore = 0
+        self.alive = True
+        self.quit = False
+        self.playerSpeed = 0
+        self.onGround = 1
+        self.blockDist = 0
+        self.nextBlockPos = 0
+        self.modOut1 = 0
+        self.modOut2 = 0
+        self.modOut3 = 0
+        self.finalOutput = 0
+        self.key = 100
+        self.min1 = 5
+        self.min2 = 5
+        self.min3 = 5
+        self.max1 = 9
+        self.max2 = 9
+        self.max3 = 9
+        self.hiddenMod1 = random.uniform(self.min1, self.max1)
+        self.hiddenMod2 = random.uniform(self.min2, self.max2)
+        self.hiddenMod3 = random.uniform(self.min3, self.max3)
+        self.hiddenMods = [self.hiddenMod1, self.hiddenMod2, self.hiddenMod3]
+
+    def returnMods(self):
+        return self.hiddenMods
+
+    def checkState(self, player):
+        self.alive = player.collisionDetect
+        if(self.alive == True):
+            self.aiScore = player.score
+            return True
+        else:
+            return False
+
+    def run(self, player, block1, block2):
+        self.playerSpeed = block1.speed
+        if(player.grounded):
+            self.onGround = 1
+        else:
+            self.onGround = 0
+        if(block1.xPos < block2.xPos):
+            self.nextBlockPos = block1.xPos
+        else:
+            self.nextBlockPos = block2.xPos
+        self.blockDist = self.nextBlockPos - player.dinoRect.right
+
+        # hidden nodes
+        self.modOut1 = self.blockDist * self.playerSpeed + self.onGround
+        self.modOut2 = self.blockDist + self.playerSpeed - self.onGround
+        self.modOut3 = self.blockDist - self.playerSpeed + self.onGround
+
+        # output layer
+        self.modOut1 = self.modOut1 * self.hiddenMod1
+        self.modOut2 = self.modOut2 * self.hiddenMod2
+        self.modOut3 = self.modOut3 * self.hiddenMod3
+        self.finalOutput = self.modOut1 - self.modOut2 - self.modOut3
+        if(self.finalOutput < self.key):
+            self.finalOutput = 1
+        else:
+            self.finalOutput = 0
+
+class learningModule():
+    def __init__(self, oldMod):
+        self.oldMod = [None, None, None]
+    def run(self):
+        for i in range(3):
+            self.oldMod[i] = oldMod[i]
+    def improveNodes(ai):
+        previousMods = ai.returnMods()
 
 block1 = enemy(1000)
 block2 = enemy(1500)
 blocks = [block1, block2]
 player = controller()
+ai = ai()
 
 class scoreThread(threading.Thread):
     def __init__(self, Name, ID):
@@ -130,6 +201,7 @@ def main():
                 #continue
             #if player.grounded and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 #player.jump()
+        ai.run(player, block1, block2)
         if(ai.finalOutput == 1):
             player.jump()
         for i in blocks:
@@ -141,8 +213,7 @@ def main():
         pygame.display.flip()
         # this updates graphics, pygame is buffered and switches around buffers
         pygame.time.wait(40)
-        # this limits game to 50 updates/sec
-
+        # this limits game to ?? updates/sec
 
 if __name__ == "__main__":
     main()
